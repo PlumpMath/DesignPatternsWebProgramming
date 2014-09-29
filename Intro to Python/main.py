2f5ef3d9463656895825857e79863c6d9c065286
Stacy Faude <istacy@fullsail.edu>
2014-09-29 18:44:27 -0500

    main.py age

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -6,5 +6,11 @@ Doc Strings
 first_name = "Kermit"
 last_name = "De Frog"
 
-response = raw_input("Enter your name")
-print response
+#response = raw_input("Enter your name")
+#print "Hello there, ", response
+
+birth_year = 1985
+current_year = 2014
+age = current_year - birth_year
+print age
+
