b36272f8d7c6d9a5bbf3ea6ff1662a57de560562
Stacy Faude <istacy@fullsail.edu>
2014-10-02 17:10:23 -0500

    Start of Madlib Program

---
+++ Intro to Python/madlib.py
@@ -0,0 +1,15 @@
+response1 = raw_input("Enter time of Day ex. morning noon or night")
+response2 = raw_input("Enter a number")
+response3 = raw_input("")
+response4 = raw_input("")
+response5 = raw_input("")
+response6 = raw_input("")
+response7 = raw_input("")
+response8 = raw_input("")
+response9 = raw_input("")
+response10 = raw_input("")
+response11 = raw_input("")
+response12 = raw_input("")
+
+print "We both lie silently still in the dead of the, ", response1
+print "Although we both lie close together we feel" + response2 + " miles apart inside"
