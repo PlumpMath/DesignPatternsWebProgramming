c649c8e0274d51c4f530de0970086e6ef519d95e
Stacy Faude <istacy@fullsail.edu>
2014-10-02 18:08:27 -0500

    Madlib If Statement
    I added an if statement

--- Intro to Python/madlib.py
+++ Intro to Python/madlib.py
@@ -1,6 +1,6 @@
-response1 = raw_input("Enter time of Day ex. morning noon or night")
-response2 = raw_input("Enter a number")
-response3 = raw_input("")
+response1 = raw_input("Enter time of Day ex. morning noon or night ")
+response2 = raw_input("Enter a number ")
+response3 = raw_input("Was it something I said or something I did? ")
 response4 = raw_input("")
 response5 = raw_input("")
 response6 = raw_input("")
@@ -11,5 +11,18 @@ response10 = raw_input("")
 response11 = raw_input("")
 response12 = raw_input("")
 
+print "Your lyrics to Every Rose Has its Thorn by Poison"
 print "We both lie silently still in the dead of the, ", response1
 print "Although we both lie close together we feel" + response2 + " miles apart inside"
+pass
+response4 = said
+
+
+if response4 > said:
+    word = "said"
+    print "I know it was something I  " + word
+elif budget > did:
+    print "I know it was something I did"
+
+else:
+    pass
