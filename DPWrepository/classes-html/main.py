866a0bae9129bbbda6d0c4cb5b3c48298d4a6cc7
Stacy Faude <istacy@fullsail.edu>
2014-10-04 10:20:39 -0500

    Classes HTML and CSS

---
+++ DPWrepository/classes-html/main.py
@@ -0,0 +1,19 @@
+'''
+Stacy Faude
+10-4-14
+class-html
+'''
+import webapp2
+from pages import Page #from package inport Class
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        #self.response.write('Hello world!')
+        p = Page()
+        p.body = "This is Stacys Python Example"
+        self.response.write(p.print_out())
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
