3bf56c226fa57011c147bf7c5c4429b756fed682
Stacy Faude <istacy@fullsail.edu>
2014-10-08 19:38:15 -0500

    Simple Login - Start

---
+++ Simple Login/simple-login/main.py
@@ -0,0 +1,15 @@
+'''
+Stacy Faude
+10-8-14
+Design Patterns for Web Programming
+Simple Form
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
