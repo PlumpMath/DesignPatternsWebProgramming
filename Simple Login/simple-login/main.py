c753118f9d02da0b67a7e53e9b56f901c0ff792c
Stacy Faude <istacy@fullsail.edu>
2014-10-08 19:40:27 -0500

    Simple-Login
    added comments

--- Simple Login/simple-login/main.py
+++ Simple Login/simple-login/main.py
@@ -4,12 +4,19 @@ Stacy Faude
 Design Patterns for Web Programming
 Simple Form
 '''
-import webapp2
+import webapp2 #use the webapp2 library
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
+class MainHandler(webapp2.RequestHandler):#declaring a class
+    def get(self): #function that starts everything. Catalyst
         self.response.write('Hello world!')
+        #code goes here
 
+    def additional_functions(self):
+        pass
+        #code goes here
+
+
+#never tough this
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
