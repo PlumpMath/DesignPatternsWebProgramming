3d17080ca37fb872eb9ae29890e311475bb74cf7
Stacy Faude <istacy@fullsail.edu>
2014-10-15 19:08:47 -0500

    Library
    Changing the form

--- library/main.py
+++ library/main.py
@@ -20,7 +20,8 @@ class MainHandler(webapp2.RequestHandler):
         <h1>Order your sandwich</h1>
         <hr>
             <label>Name: </label><input type="text" name="user" /><br/>
-            <label>Email: </label><input type="text" name="email" /><br/>
+            <label>Phone: </label><input type="text" name="phone" /><br/>
+            <p>Type of Bread?</p>
             <input type="checkbox" name="bread" value="white" /> White<br/>
             <input type="checkbox" name="bread" value="wheat" /> Wheat<br/>
             <input type="checkbox" name="bread" value="flatbread" /> Flat Bread<br/>
@@ -28,21 +29,16 @@ class MainHandler(webapp2.RequestHandler):
             <select name="sandwich">
                 <option value="Pompeii">Pompeii</option>
                 <option value="Titan">Titan</option>
-                <option value="Erupter">Spartan</option>
-                <option value="Erupter">Spartan</option>
+                <option value="Erupter">Erupter</option>
+                <option value="Spartan">Spartan</option>
                 <option value="Quatro">Quatro</option>
                 <option value="Apollo">Apollo</option>
             </select>
-            <p>Soup of the Day?</p>
-            <select name="soup">
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
-            </select>
-            <p>Chips</p>
-             <select name="chips">
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
-            </select>
+            <p>Quantity</p>
+            <input type="number" name="quantity" min="1" max="20" />
+            <p>Chips?</p>
+             <input type="radio" name="chips" value="yes">yes
+             <input type="radio" name="chips" value="no">no
             <br/>
             <input type="submit" value="Submit" />'''
         page_close = '''
@@ -53,12 +49,12 @@ class MainHandler(webapp2.RequestHandler):
         if self.request.GET:
             #stores info we got from the form
             user = self.request.GET['user']
-            email = self.request.GET['email']
+            phone = self.request.GET['phone']
             bread = self.request.GET['bread']
             sandwich = self.request.GET['sandwich']
             soup = self.request.GET['soup']
             chips = self.request.GET['chips']
-            self.response.write(page_head + user + ' ' + email + ' ' + bread + ' ' + sandwich + ' ' + soup + ' ' + chips + page_close)
+            self.response.write(page_head + user + ' ' + phone + ' ' + bread + ' ' + sandwich + ' ' + soup + ' ' + chips + page_close)
         else:
             self.response.write(page_head + page_body + page_close) #print
 
---
+++ library/resultsPage.py
@@ -0,0 +1,22 @@
+class Page(object):
+    def __init__(self):
+        self.__title = "Stacy Faude"
+        self.css = "css/styles.css"
+        self.__head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Enter your information:</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        """
+        self.body = ""
+        self.__error = ' '
+        self.__close = """
+    </body>
+</html>
+        """
+    def print_out(self):
+        all = self.__head + self.body + self.__error + self.__close
+        return all
