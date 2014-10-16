f8f1abcbff63f6c07b1067d6e833e04db3054a36
Stacy Faude <istacy@fullsail.edu>
2014-10-16 14:46:49 -0500

    Library
    Not sure what to to next…

--- library/library.py
+++ library/library.py
@@ -4,23 +4,5 @@ class OrderSandwich(object):
         self.phone = ''
         self.bread = ''
         self.sandwich = ''
-        self.quantity = '' #check for valid quantity
+        self.__quantity = '' #check for valid quantity
         self.chips = ''
-
-
-    def add_order(self, s):
-        self.__your_order(s)
-
-
-    def compile_order(self):
-        output = ''
-        for order in self.__your_order:
-            output += 'Your Name: ' + ' ' + 'You wanted ' + '<br />'
-        return output
-
-
-
-    def calc_order_total(self):
-        '''
-        calculate the total of the order
-        '''
